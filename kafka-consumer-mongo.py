2a4c5c7fe1fc844c8191ef0de946aec29a4c829e
juan <juand_david2001@hotmail.com>
2023-05-30 00:49:01 -0600

    se realizan cambios

--- kafka-consumer-mongo.py
+++ kafka-consumer-mongo.py
@@ -56,7 +56,7 @@ for msg in consumerReactions:
         agg_result = db.reactions_info.aggregate(
             [{
                 "$group" : { "_id" : "$publication",
-                             [{"$group" : { "_id" : "$reaction",
+                             "reactions":[{"$group" : { "_id" : "$reaction",
                                           "total" : {"$sum":1}
                                         }}],
                              "total":{"$sum":1}
